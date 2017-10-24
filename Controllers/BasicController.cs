f4300093c52112a3e54b68a84f9d04161a3b8eb2
NorbertPinczes <norbert.pinczes@yahoo.com>
2017-10-24 16:23:58 +0100

    Add files via upload

---
+++ Controllers/BasicController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyMVC.Controllers
+{
+    public class BasicController : Controller
+    {
+        //
+        // GET: /Basic/
+
+        public ActionResult Basic()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyMVC.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //public string Index()
+        //{
+        //    return "This is my <b>default</b> action...";
+        //}
+
+        //
+        // GET: /HelloWorld/Welcome/
+
+        public ActionResult Welcome(string name, int num = 1)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = num;
+
+            return View();
+            //return "This is the Welcome action method..." + name + " - " + num;
+        }
+
+        public ActionResult Basic1()
+        {
+            return View();
+        }
+
+        public ActionResult Basic2()
+        {
+            return View();
+        }
+
+    }
+}
